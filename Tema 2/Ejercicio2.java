1ead423f2a268059c7c31035cc963a3e4a60cd22
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:20:34 +0100

    Update Ejercicio2.java

--- Tema 2/Ejercicio2.java
+++ Tema 2/Ejercicio2.java
@@ -1,3 +1,5 @@
+/* Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por pantalla */
+
 public class Ejercicio2 {
     public static void main(String args[]) { 
       String name = "Hari";
