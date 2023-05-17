d262b248d444cd181d5372da2564ec9e911628d9
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:35:04 +0100

    Create Ejercicio8.java

---
+++ Tema 3/Ejercicio8.java
@@ -0,0 +1,11 @@
+/* Escribe un programa que calcule el salario semanal de un empleado en base a las horas trabajadas, a razón de 12 euros la hora. */
+
+public class Ejercicio8 {
+    public static void main(String args[]) {
+      Scanner trabajo = new Scanner(System.in);
+      int salario = 12;
+      System.out.println("Introduce las horas trabajadas esta semana: ");
+      int horas = trabajo.nextInt();
+      System.out.println("Esta semana cobras: " + (horas * salario) + "€");
+    }
+}
