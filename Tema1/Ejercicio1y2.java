e5ebe516a33ca03bc09410f6f9135e218e77a0c7
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:06:53 +0100

    Create Ejercicio1y2.java

---
+++ Tema1/Ejercicio1y2.java
@@ -0,0 +1,22 @@
+public class Ejercicio1y2 {
+    public static void main(String args[]) {
+      //Ejercicio 1
+      System.out.println("Mi nombre es Hari");
+      System.out.println("Mi telefono es 6464414");
+      System.out.println("Mi dirección es Calle pepito");
+      System.out.println();
+      
+      //Ejercicio 2
+      System.out.printf("%-10s  %s\n", "computer", "ordenador");
+      System.out.printf("%-10s  %s\n", "student", "alumno");
+      System.out.printf("%-10s  %s\n", "cat", "gato");
+      System.out.printf("%-10s  %s\n", "penguin", "pingüino");
+      System.out.printf("%-10s  %s\n", "machine", "maquina");
+      System.out.printf("%-10s  %s\n", "nature", "naturaleza");
+      System.out.printf("%-10s  %s\n", "light", "luz");
+      System.out.printf("%-10s  %s\n", "green", "verde");
+      System.out.printf("%-10s  %s\n", "book", "libro");
+      System.out.printf("%-10s  %s\n", "dog", "perro");
+      System.out.println();
+    }
+}
