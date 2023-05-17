07f16337bb7c0ba91ff4fbb9ce2f2632f7b8606d
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:25:47 +0100

    Create Ejercicio1.java

---
+++ Tema 3/Ejercicio1.java
@@ -0,0 +1,11 @@
+/* Realiza un programa que pida dos números y que luego muestre el resultado de su multiplicación. */
+
+public class Ejercicio1 {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa dos numeros: ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println("La multiplicacion es: " + x * y);
+    }
+}
