d8ee64c510a2b74e7ed8c5871c91c9dea955d58d
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:14:52 +0100

    Create Ejercicio5.java

---
+++ Tema 2/Ejercicio5.java
@@ -0,0 +1,11 @@
+/* Realiza un conversor de pesetas a euros. La cantidad en pesetas que se quiere
+convertir deberá estar almacenada en una variable. */
+
+public class Ejercicio5 {
+    public static void main(String args[]) {      
+      double euro1 = 0.006;
+      double peseta1 = 400;
+      System.out.println(peseta1 * euro1);
+      System.out.println();
+    }
+}
