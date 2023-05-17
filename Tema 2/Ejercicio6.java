eb876fd00bc1e91887584be5507dabcbd90acce1
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:44:28 +0100

    Update Ejercicio6.java

--- Tema 2/Ejercicio6.java
+++ Tema 2/Ejercicio6.java
@@ -1,5 +1,5 @@
-/* Escribe un programa que calcule el total de una factura a partir de la baseimponible (precio sin IVA).
-La base imponible estará almacenada en una variable. */
+/* Escribe un programa que calcule el total de una factura a partir de la 
+baseimponible (precio sin IVA). La base imponible estará almacenada en una variable. */
 
 public class Ejercicio6 {
     public static void main(String args[]) { 
