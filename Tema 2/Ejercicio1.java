01cd32bf38829783c22f0f94421e7ff1d66cec41
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:10:10 +0100

    Create Ejercicio1.java

---
+++ Tema 2/Ejercicio1.java
@@ -0,0 +1,13 @@
+public class EjerciciosVariables {
+    public static void main(String args[]) {
+      int x = 144;
+      int y = 999;
+      System.out.println(x);
+      System.out.println(y);
+      System.out.println(x + y);
+      System.out.println(x - y);
+      System.out.println(x / y);
+      System.out.println(x * y );
+      System.out.println();
+    }
+}
