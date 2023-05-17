67dd581c647ed789a510c03276410a627cf31abf
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:44:00 +0100

    Update Ejercicio2.java

--- Tema 2/Ejercicio2.java
+++ Tema 2/Ejercicio2.java
@@ -1,4 +1,5 @@
-/* Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por pantalla */
+/* Crea la variable nombre y asígnale tu nombre completo. Muestra su valor 
+por pantalla */
 
 public class Ejercicio2 {
     public static void main(String args[]) { 
