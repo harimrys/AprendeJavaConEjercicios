b08818c2706977af52ccee9fecd001a0478f1232
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:36:53 +0100

    Create Ejercicio1.java

---
+++ Tema 4/Ejercicio1.java
@@ -0,0 +1,45 @@
+/* Escribe un programa que pida por teclado un día de la semana y que diga qué asignatura toca a primera hora ese día. */
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduce un dia de la semana");
+        String dia = sc.nextLine();
+        
+        switch(dia){
+            case "lunes":
+                System.out.println("Hoy es lunes!");
+                break;
+            
+            case "martes":
+                System.out.println("Hoy es martes!");
+                break;
+            
+            case "miercoles":
+                System.out.println("Hoy es miercoles!");
+                break;
+            
+            case "jueves":
+                System.out.println("Hoy es jueves");
+                break;
+
+            case "viernes":
+                System.out.println("Hoy es viernes BIEEEEN!");
+                break;
+
+            case "sabado":
+                System.out.println("Hoy es sabado!!");
+                break;
+
+            case "domingo":
+                System.out.println("Oh no! hoy es domingo :(");
+                break;
+            
+            default:
+                System.out.println("Introduce un dia de la semana valido");
+        } 
+    }
+}
