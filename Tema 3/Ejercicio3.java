e0d937daaf0d4b28e4b6b1ec59f01aafa26914b9
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:39:29 +0100

    Update Ejercicio3.java

--- Tema 3/Ejercicio3.java
+++ Tema 3/Ejercicio3.java
@@ -1,4 +1,5 @@
 /* Realiza un conversor de pesetas a euros. La cantidad de pesetas que se quiere convertir debe ser introducida por teclado. */
+import java.util.Scanner;
 
 public class Ejercicio3 {
     public static void main(String args[]) {
