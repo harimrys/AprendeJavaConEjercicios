55ab548dd7371d56c3bc570130de7f7d5decf8f7
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:30:47 +0100

    Create Ejercicio4.java

---
+++ Tema 3/Ejercicio4.java
@@ -0,0 +1,14 @@
+/* Escribe un programa que sume, reste, multiplique y divida dos números introducidos por teclado. */
+
+public class Ejercicio4 {
+    public static void main(String args[]) {
+      Scanner num = new Scanner(System.in);
+      System.out.println("Introuce dos numeros: ");
+      int a = num.nextInt();
+      int b = num.nextInt();
+      System.out.println("La suma de los dos numeros es: " + (a + b));
+      System.out.println("La resta de los dos numeros es: " + (a - b));
+      System.out.println("La multiplicacion de los dos numeros es: " + (a * b));
+      System.out.println("La division de los dos numeros es: " + (a / b));
+    }
+}
