68659772a7d26290af86e914701108f909f8104a
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:33:10 +0100

    Create Ejercicio6.java

---
+++ Tema 3/Ejercicio6.java
@@ -0,0 +1,12 @@
+/* Escribe un programa que calcule el área de un triángulo. */
+
+public class Ejercicio6 {
+    public static void main(String args[]) {
+      Scanner trian = new Scanner(System.in);
+      System.out.println("Introduce la base: ");
+      int base1 = trian.nextInt();
+      System.out.println("Introduce la altura: ");
+      int altura1 = trian.nextInt();
+      System.out.println("El area del triangulo es: " + (base1 * altura1 /2 )); 
+    }
+}
