5ddf7f203bf9c0b00d06ce9331bcbe7c0875d87c
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:37:44 +0100

    Create Ejercicio2.java

---
+++ Tema 1/Ejercicio2.java
@@ -0,0 +1,18 @@
+/* Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su correspondiente traducción 
+al castellano. Las palabras deben estar distribuidas en dos columnas y alineadas a la izquierda. */
+
+public class Ejercicio2 {
+    public static void main(String args[]) {
+
+      System.out.printf("%-10s  %s\n", "computer", "ordenador");
+      System.out.printf("%-10s  %s\n", "student", "alumno");
+      System.out.printf("%-10s  %s\n", "cat", "gato");
+      System.out.printf("%-10s  %s\n", "penguin", "pingüino");
+      System.out.printf("%-10s  %s\n", "machine", "maquina");
+      System.out.printf("%-10s  %s\n", "nature", "naturaleza");
+      System.out.printf("%-10s  %s\n", "light", "luz");
+      System.out.printf("%-10s  %s\n", "green", "verde");
+      System.out.printf("%-10s  %s\n", "book", "libro");
+      System.out.printf("%-10s  %s\n", "dog", "perro");
+    }
+}
