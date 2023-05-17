ad2ec663869f470b6fd3f90873e97f3e0c88cd32
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:17:39 +0100

    Create Ejercicio7.java

---
+++ Tema 2/Ejercicio7.java
@@ -0,0 +1,11 @@
+/* Escribe un programa que declare variables de tipo char y de tipo String. Intenta mostrarlas por pantalla
+todas juntas en la misma línea y con una sola sentencia de Java (con un solo println) ¿es posible? */
+
+
+public class Ejercicio6 {
+    public static void main(String args[]) { 
+      char abc = 'A';
+      String abc1 = "abc";
+      System.out.println(abc + " " + abc1);
+    }
+}
