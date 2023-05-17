573c064614d49ca3d804735ed2e7aadf60d1eafe
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 14:22:54 +0100

    Update and rename Ejercicio1y2.java to Ejercicio1.java

---
+++ Tema1/Ejercicio1.java
@@ -0,0 +1,10 @@
+/* Escribe una programa que muestre tu nombre por pantalla, muestre tu dirección y
+tu número de teléfono. Asegúrate de que los datos se muestran en líneas separadas. */
+
+public class Ejercicio1 {
+    public static void main(String args[]) {
+      System.out.println("Mi nombre es Hari");
+      System.out.println("Mi telefono es 6111222414");
+      System.out.println("Mi dirección es Calle pepito");
+    }
+}
