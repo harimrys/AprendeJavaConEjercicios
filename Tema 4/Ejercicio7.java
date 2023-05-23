88df6ab849245cc9d75dbb03fc37fa3ee95dd10a
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-23 13:03:34 +0100

    Create Ejercicio7.java

---
+++ Tema 4/Ejercicio7.java
@@ -0,0 +1,22 @@
+/* Realiza un programa que calcule la media de tres notas. */
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Introduce la primera nota: ");
+        double a = sc.nextDouble();
+        
+        System.out.print("Introduce la segunda nota: ");
+        double b = sc.nextDouble();
+        
+        System.out.print("Introduce la tercera nota: ");
+        double c = sc.nextDouble();
+        
+        double media = (a + b + c) / 3;
+        
+        System.out.printf("La nota media es: %.2f \n", media);
+    }
+}
