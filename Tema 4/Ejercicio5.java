764629ed5144a620279a473c9c301db3369aa7b7
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-23 12:51:00 +0100

    Create Ejercicio5.java

---
+++ Tema 4/Ejercicio5.java
@@ -0,0 +1,24 @@
+/* Realiza un programa que resuelva una ecuación de primer grado (del tipo ax+b =0) */
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Este programa resuelve ecuaciones de primer grado del tipo ax + b = 0");
+        
+        System.out.print("Introduce el valor de a: ");
+        double a = sc.nextDouble();
+        
+        System.out.print("Introduce el valor de b: ");
+        double b = sc.nextDouble();
+        
+        if(a == 0){
+            System.out.println("Esta ecuación no tiene solución");
+        } else {
+            double ecuacion = -b/a;
+            System.out.println("x = " + ecuacion);
+        }
+    }
+}
