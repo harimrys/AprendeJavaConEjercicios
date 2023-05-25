af53513776991bb5e4799db4d0c7efe24653a66f
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-25 10:58:08 +0100

    Create Ejercicio11.java

---
+++ Tema 4/Ejercicio11.java
@@ -0,0 +1,21 @@
+/* Escribe un programa que dada una hora determinada (horas y minutos), calcule
+los segundos que faltan para llegar a la medianoche. */
+
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Introduce una hora: ");
+        int hora = sc.nextInt();
+        
+        System.out.print("Introduce los minutos: ");
+        int min = sc.nextInt();
+        
+        int segundos = (hora * 3600) + (min * 60);
+        int segundosHastaMedianoche = (24 * 3600) - segundos;
+        
+        System.out.println("Son las " + hora + ":" + min + " hasta medianoche faltan " + segundosHastaMedianoche + " segundos.");
+    }
+}
